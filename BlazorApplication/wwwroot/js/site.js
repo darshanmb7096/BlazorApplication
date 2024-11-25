e885463fa6320b48a61cf80898d9c173b38261cc
DarshanMB <morkandadarshan@gmail.com>
2024-11-25 17:23:01 +0530

    Add project files.

---
+++ BlazorApplication/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// wwwroot/js/site.js
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
+    }
+}
+
+function hideModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
